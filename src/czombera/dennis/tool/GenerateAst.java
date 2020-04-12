ee194433f6681ea509b1ed2562c72cf75a4e5876
Dennis Czombera <milchschaumschluerfer@gmail.com>
2020-04-12 22:33:43 +0200

    Add vistor pattern to generated ast nodes

--- src/czombera/dennis/tool/GenerateAst.java
+++ src/czombera/dennis/tool/GenerateAst.java
@@ -2,6 +2,7 @@ package czombera.dennis.tool;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,13 +24,14 @@ public class GenerateAst {
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
         String path = outputDir + "/" + baseName + ".java";
-        PrintWriter writer = new PrintWriter(path, "UTF-8");
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
 
         writer.println("package czombera.dennis.lox;");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+        defineVisitor(writer, baseName, types);
 
         for (String type : types) {
             String className = type.split(":")[0].trim();
@@ -37,8 +39,25 @@ public class GenerateAst {
             defineType(writer, baseName, className, fields);
         }
 
+        // The base accept() method.
+        writer.println();
+        writer.println("abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("}");
     }
 
 
@@ -59,6 +78,13 @@ public class GenerateAst {
         }
 
         writer.println("    }");
+
+        // Visitor pattern.
+        writer.println();
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("}");
+
 
         // Fields.
         writer.println();
