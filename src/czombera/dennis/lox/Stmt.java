fb01f5c1f33c9fa8b8fdebf9362a38a7cc88572e
Dennis Czombera <milchschaumschluerfer@gmail.com>
2020-04-19 15:42:53 +0200

    Add AST node for while statement

--- src/czombera/dennis/lox/Stmt.java
+++ src/czombera/dennis/lox/Stmt.java
@@ -5,72 +5,97 @@ import java.util.List;
 abstract class Stmt {
     interface Visitor<R> {
         R visitBlockStmt(Block stmt);
+
         R visitExpressionStmt(Expression stmt);
+
         R visitIfStmt(If stmt);
+
+        R visitWhileStmt(While stmt);
+
         R visitPrintStmt(Print stmt);
+
         R visitVarStmt(Var stmt);
-}
-  static class Block extends Stmt {
-    Block(List<Stmt> statements) {
-      this.statements = statements;
     }
+
+    static class Block extends Stmt {
+        Block(List<Stmt> statements) {
+            this.statements = statements;
+        }
 
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitBlockStmt(this);
-}
+        }
 
-    final List<Stmt> statements;
-  }
-  static class Expression extends Stmt {
-    Expression(Expr expression) {
-      this.expression = expression;
+        final List<Stmt> statements;
     }
+
+    static class Expression extends Stmt {
+        Expression(Expr expression) {
+            this.expression = expression;
+        }
 
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitExpressionStmt(this);
-}
+        }
 
-    final Expr expression;
-  }
-  static class If extends Stmt {
-    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
-      this.condition = condition;
-      this.thenBranch = thenBranch;
-      this.elseBranch = elseBranch;
+        final Expr expression;
     }
+
+    static class If extends Stmt {
+        If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+            this.condition = condition;
+            this.thenBranch = thenBranch;
+            this.elseBranch = elseBranch;
+        }
 
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitIfStmt(this);
-}
+        }
 
-    final Expr condition;
-    final Stmt thenBranch;
-    final Stmt elseBranch;
-  }
-  static class Print extends Stmt {
-    Print(Expr expression) {
-      this.expression = expression;
+        final Expr condition;
+        final Stmt thenBranch;
+        final Stmt elseBranch;
     }
+
+    static class While extends Stmt {
+        While(Expr condition, Stmt body) {
+            this.condition = condition;
+            this.body = body;
+        }
+
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitWhileStmt(this);
+        }
+
+        final Expr condition;
+        final Stmt body;
+    }
+
+    static class Print extends Stmt {
+        Print(Expr expression) {
+            this.expression = expression;
+        }
 
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitPrintStmt(this);
-}
+        }
 
-    final Expr expression;
-  }
-  static class Var extends Stmt {
-    Var(Token name, Expr initializer) {
-      this.name = name;
-      this.initializer = initializer;
+        final Expr expression;
     }
+
+    static class Var extends Stmt {
+        Var(Token name, Expr initializer) {
+            this.name = name;
+            this.initializer = initializer;
+        }
 
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitVarStmt(this);
-}
+        }
 
-    final Token name;
-    final Expr initializer;
-  }
+        final Token name;
+        final Expr initializer;
+    }
 
-abstract <R> R accept(Visitor<R> visitor);
+    abstract <R> R accept(Visitor<R> visitor);
 }
