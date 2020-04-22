a95daa4e3971daaa48c4e3814b52e618ce63498b
Dennis Czombera <milchschaumschluerfer@gmail.com>
2020-04-22 10:41:47 +0200

    Add initial step for anonymous function expressions

--- src/czombera/dennis/tool/GenerateAst.java
+++ src/czombera/dennis/tool/GenerateAst.java
@@ -18,6 +18,8 @@ public class GenerateAst {
                 "Assign   : Token name, Expr value ",
                 "Binary   : Expr left, Token operator, Expr right",
                 "Call     : Expr callee, Token paren, List<Expr> arguments",
+                // TODO: Add parsing and interpretation for anonymous functions
+                // "Function : List<Token> params, List<Stmt> body",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
                 "Logical  : Expr left, Token operator, Expr right",
